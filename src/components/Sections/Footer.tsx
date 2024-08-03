da0b0efa5f9506c4f4e271d32de4ec19f3fb9300
Veeresh Kande <veereshkande@Veereshs-MacBook-Pro.local>
2024-08-03 17:11:31 -0500

    updated imports in footer

--- src/components/Sections/Footer.tsx
+++ src/components/Sections/Footer.tsx
@@ -1,4 +1,5 @@
-import { BoltIcon, ChevronUpIcon } from '@heroicons/react/24/solid';
+// import { BoltIcon, ChevronUpIcon } from '@heroicons/react/24/solid';
+import { ChevronUpIcon } from '@heroicons/react/24/outline';
 import { FC, memo } from 'react';
 
 import { SectionId } from '../../data/data';
